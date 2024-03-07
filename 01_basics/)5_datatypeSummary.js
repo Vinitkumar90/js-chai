b0f655eb5bc2636cb1b4c17f5e2f14c18f32c994
vinit6141 <106748541+Vinitkumar90@users.noreply.github.com>
2024-03-07 10:10:11 +0000

    dataTypeSummary

---
+++ 01_basics/)5_datatypeSummary.js
@@ -0,0 +1,14 @@
+//datatypes are divided into two types :
+// according to how they get 1>stored in memory and how we can 2>access
+
+// 1> Primtive  (7 Types:) all primitive data types are call by value : means when
+//ever we call them any where ...we dont get there memory reference ...we get a copy and all the changes reflect to copy and not original data
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+
+// 2> refrence type of (non-primitve) data-type
+// Array, Objects, Functions
+
+// javascript is dynamically typed or static typed language
+// ----> JavaScript is a dynamically typed language
