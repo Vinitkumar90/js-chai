001051ee882f090901a611f39bfa77e8a17d0121
vinit6141 <106748541+Vinitkumar90@users.noreply.github.com>
2024-03-07 09:26:44 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,41 @@
+let score = "33av";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let scoreInNumber = Number(score);
+//console.log(typeof scoreInNumber);
+//console.log(scoreInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false  //empty string
+// "hitesh" => true
+
+let somCoolNumber = 88;
+let someCoolString = String(somCoolNumber);
+//console.log(someCoolString)
+//console.log(typeof someCoolString)
+
+//operations
+
+let str1 = "vinit"
+let str2 = "kumar"
+
+let str3=  str1+str2
+//console.log(str3)
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
