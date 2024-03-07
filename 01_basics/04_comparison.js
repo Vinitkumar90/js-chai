b09ae09ca2fcab924cf99c54f38f47a8bafa84b9
vinit6141 <106748541+Vinitkumar90@users.noreply.github.com>
2024-03-07 09:50:58 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+//comparisonOperartions
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//console.log("2">1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log("2" === 2) 
+// now here no conversion will take place...first same datatype for comparison
